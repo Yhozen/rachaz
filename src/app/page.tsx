eaa9f48b6d59525a2643fae1d5178135e557f099
Gabriel Pérez <gabriel@garox.org>
2025-02-08 22:03:31 -0300

    add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { prisma } from "@/db/client";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const restaurants = await prisma.restaraunt.findMany();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <pre>{JSON.stringify({ restaurants }, null, 2)}</pre>
         <Image
           className="dark:invert"
           src="/next.svg"
